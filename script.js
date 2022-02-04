119d24dec9871facfd6887f6bd4a8a46f60cd7ef
Shaswat Raj <66713844+SH20RAJ@users.noreply.github.com>
2022-02-04 04:49:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let s = (a) => document.getElementById(a);
+let categories = WebScrapper.getjson('https://wholly-api.herokuapp.com/websites/pngaaa.com/categories.php');
+
+let c=a=>console.log(a);
+//Functions
+let getsearch = (cat, page) => {
+  let nature = WebScrapper.getjson('https://wholly-api.herokuapp.com/websites/pngaaa.com/search.php?q=' + cat + '&page=' + page + '');
+  return nature;
+}
+
+let showresult = (id,cat) => {
+  let result = getsearch(cat);
+  c(result);
+  for (let i = 0; i < result.data.length; i++) {
+    let url = 'https://image.pngaaa.com/' + result.data[i].substr(-3, 3) +'/'+ result.data[i]+'-middle.png';
+    let caption = 'nature';
+
+    let html = `<figure style="background-image: url(${url})">
+    <figcaption>${caption}</figcaption>
+  </figure>`;
+    console.log(html);
+    s(id).insertAdjacentHTML('beforeend', html);
+  }
+
+}
+showresult('nature','main');
+
+function showcat(id, a) {
+  if (!a) a = categories.data.length;
+  for (let i = 0; i < a; i++) {
+    let cat = categories.data[i];
+    s(id).insertAdjacentHTML('beforeend', '<article>' + cat + '</article>');
+  }
+}
+showcat('folders', 3);
+showcat('folders2');
