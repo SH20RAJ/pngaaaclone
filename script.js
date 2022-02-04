7b448f9cb8343a60cc5533a271eda53ba3d06b55
Shaswat Raj <66713844+SH20RAJ@users.noreply.github.com>
2022-02-04 06:10:21 +0000

    All Fixed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let weburl =()=> location.search.split(/[?#]/)[0];
 
 let s = (a) => document.getElementById(a);
-let categories = WebScrapper.getjson('https://wholly-api.herokuapp.com/websites/pngaaa.com/categories.php');
+let categories = WebScrapper.fetchjson('https://wholly-api.herokuapp.com/websites/pngaaa.com/categories.php');
 let bydefault = 'nature';
 
 let searchparam = WebScrapper.getparam;
@@ -9,28 +9,28 @@ let searchparam = WebScrapper.getparam;
 let c=a=>console.log(a);
 //Functions
 let getsearch = (cat, page) => {
-  let nature = WebScrapper.getjson('https://wholly-api.herokuapp.com/websites/pngaaa.com/search.php?q=' + cat + '&page=' + page + '');
+  let url = 'https://wholly-api.herokuapp.com/websites/pngaaa.com/search.php?q=' + cat + '&page=' + page + '';
+  let nature = WebScrapper.fetchjson(url);
+  c(url);
   return nature;
 }
 
-let showresult = (id,cat) => {
-  let result = getsearch(cat);
+let showresult = (id,cat,page) => {
+  let result = getsearch(cat,page);
   c(result);
   for (let i = 0; i < result.data.length; i++) {
     let url = 'https://image.pngaaa.com/' + result.data[i].substr(-3, 3) +'/'+ result.data[i]+'-middle.png';
-    let caption = 'nature';
 
     let html = `<figure style="background-image: url(${url})">
     <figcaption><a href ="https://www.pngaaa.com/api-download/${result.data[i]}">Download</a> | <a href ="${url}" onclick="view(this.href)">View</a></figcaption>
   </figure>`;
-    console.log(html);
     s(id).insertAdjacentHTML('beforeend', html);
   }
 
 }
 let view=()=>{};
 if(searchparam('q')){
-  showresult('main',searchparam('q'));
+  showresult('main',searchparam('q'),searchparam('page'));
   s('q').value = searchparam('q');
   s('page').value = searchparam('page');
 } else {
